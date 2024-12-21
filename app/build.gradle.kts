4978b93f39e07e23344aea82c76357b6ef03675e
Ihor Oleshko <oleshko.igor94@gmail.com>
2024-12-21 16:52:17 -0500

    WT-002 added navigation compose dep

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    debugImplementation(libs.androidx.navigation.compose)
 
     implementation(project(":domain"))
     implementation(project(":data"))
